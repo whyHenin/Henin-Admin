b9c2984165edd0c30ddff4cf88516e27fb430acd
chenHome <chen.dev@foxmail.com>
2022-04-14 00:10:22 +0800

    🐳 chore(Gitcommit): 删除Gitcommitlint工具
    并不适合当前风格

--- commitlint.config.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: Chen Xin
- * @Date: 2022-04-13 23:16:30
- * @LastEditTime: 2022-04-13 23:18:02
- * @LastEditors: Chen Xin
- * @Description:
- * @FilePath: \Henin-Admin\commitlint.config.js
- */
-module.exports = { extends: ["@commitlint/config-conventional"] }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Chen Xin
  * @Date: 2022-04-11 21:33:57
- * @LastEditTime: 2022-04-13 22:45:14
+ * @LastEditTime: 2022-04-14 00:01:40
  * @LastEditors: Chen Xin
  * @Description:
  * @FilePath: \Henin-Admin\src\main.ts
@@ -12,8 +12,6 @@ import store from "./store"
 import App from "./App.vue"
 
 const app = createApp(App)
-
-const a = 123
 
 app.use(router).use(store)
 
