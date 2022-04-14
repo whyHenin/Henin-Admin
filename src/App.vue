3ec3333aa726ed702b6018866b78a2a9c4864406
Coffee-crocodile <1147347984@qq.com>
2022-04-14 08:58:39 +0800

    🌈 style(eslint.config): 关闭换行符检测

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author:ChenXin
  * @Date: 2022-03-16 09:18:55
- * @LastEditTime: 2022-04-12 21:38:45
+ * @LastEditTime: 2022-04-13 08:58:04
  * @LastEditors: Chen Xin
  * @Description: 
  * @FilePath: \Henin-Admin\src\App.vue
