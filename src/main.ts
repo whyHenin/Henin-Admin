421a48eddd0a9d93bf3fc45a1bebe8c5581f5d4d
Coffee-crocodile <1147347984@qq.com>
2022-04-15 09:01:30 +0800

    🐳 chore(tsconfig): 增加环境变量支持

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Chen Xin
  * @Date: 2022-04-11 21:33:57
- * @LastEditTime: 2022-04-14 00:01:40
+ * @LastEditTime: 2022-04-14 13:34:31
  * @LastEditors: Chen Xin
  * @Description:
  * @FilePath: \Henin-Admin\src\main.ts
