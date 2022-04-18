a232c73fcce6970f6b43f1f40c931bcc60738e56
Coffee-crocodile <1147347984@qq.com>
2022-04-18 18:17:52 +0800

    🐞 styles(header): 修改了haedert图标样式

--- src/layout/components/header/toggleSideBar.vue
+++ src/layout/components/header/toggleSideBar.vue
@@ -1,17 +1,19 @@
 <!--
  * @Author: Chen Xin
  * @Date: 2022-04-17 14:12:25
- * @LastEditTime: 2022-04-17 20:26:07
+ * @LastEditTime: 2022-04-18 09:37:57
  * @LastEditors: Chen Xin
  * @Description: 
  * @FilePath: \Henin-Admin\src\layout\components\header\toggleSideBar.vue
 -->
 
 <template>
-  <el-icon class="header-icon" @click="pageStore.setCollapse">
-    <i-ri-arrow-right-circle-line v-if="pageStore.isCollapse" />
-    <i-ri-arrow-left-circle-line v-else />
-  </el-icon>
+  <div class="header-icon" @click="pageStore.setCollapse">
+    <el-icon>
+      <i-ri-arrow-right-circle-line v-if="pageStore.isCollapse" />
+      <i-ri-arrow-left-circle-line v-else />
+    </el-icon>
+  </div>
 </template>
 
 <script setup lang="ts">
