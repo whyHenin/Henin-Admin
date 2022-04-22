f78497c28fa54be7cd1446ed500e7142e39b17b7
Coffee-crocodile <1147347984@qq.com>
2022-04-22 18:28:32 +0800

    feat(card): 测试了icon的自定义渲染导入

--- src/views/list/card/testCard.vue
+++ src/views/list/card/testCard.vue
@@ -1,17 +1,19 @@
 <!--
  * @Author: Chen Xin
  * @Date: 2022-04-22 13:40:32
- * @LastEditTime: 2022-04-22 18:10:26
+ * @LastEditTime: 2022-04-22 18:27:31
  * @LastEditors: Chen Xin
  * @Description: 
  * @FilePath: \Henin-Admin\src\views\list\card\testCard.vue
 -->
 <script lang="tsx">
-import { defineComponent, ref, h, compile, computed } from "vue"
+import { h, resolveComponent } from "vue"
 export default defineComponent({
   setup() {
-    const icon = "<icon-dashboard />"
-    return () => icon
+    const ButtonCounter = resolveComponent("icon-apps")
+    return () => {
+      return h(ButtonCounter)
+    }
   },
 })
 </script>
