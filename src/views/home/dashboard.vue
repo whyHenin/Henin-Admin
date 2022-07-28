21ca3727180524e1a9372809377516ba05b61a94
‘chen.home’ <chen.dev@foxmail.com>
2022-07-29 00:09:35 +0800

    test

--- src/views/home/dashboard.vue
+++ src/views/home/dashboard.vue
@@ -1,14 +1,14 @@
 <!--
  * @Author: Chen Xin
  * @Date: 2022-04-12 21:39:15
- * @LastEditTime: 2022-06-17 00:47:18
+ * @LastEditTime: 2022-07-29 00:09:21
  * @LastEditors: Chen Xin
  * @Description: 
  * @FilePath: \Henin-Admin\src\views\home\dashboard.vue
 -->
 <template>
   <breadcrumb />
-  <p>流量版</p>
+  <p>流量板</p>
 </template>
 
 <script lang="ts" setup>
